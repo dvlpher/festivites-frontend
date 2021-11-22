bba74bef4b83f59458d2eb4e835cf39c6674e8ee
CelesteSMThomas <celestesmthomas@gmail.com>
2021-11-22 11:21:54 -0800

    fixed issue of new trips not grabbing user id and inadvertently firing the no user logged in error prompt

--- src/components/TripForm.js
+++ src/components/TripForm.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { updateTripForm } from "../actions/tripForm.js";
-import { createTrip } from "../actions/myTrips.js";
 import { connect } from 'react-redux'
 
-const TripForm = ({ formData, history, updateTripForm, createTrip, userId}) => {
-    const { name, startDate, endDate } = formData 
+const TripForm = ({ formData, updateTripForm, userId, trip, handleSubmit, editMode  }) => {
+    const { name, startDate, endDate, festivalLength, ticketCost, travelGroup, stayName, stayAddress, stayCost   } = formData 
 
     const handleChange= event => {
         console.log("trigger handle change")
@@ -12,40 +11,38 @@ const TripForm = ({ formData, history, updateTripForm, createTrip, userId}) => {
         updateTripForm(name, value)
     }
 
-    const handleSubmit = event => {
-        event.preventDefault()
-        createTrip({
-            ...formData,
-                userId,
-            }, history)
-
-        // formData: {
-        //     name: ""
-        //     startDate: ""
-        //     endDate: ""
-        //}
-        
-    }
-
     
     return( 
-    <form onSubmit={handleSubmit}> 
+        <form onSubmit={event => {
+            event.preventDefault()
+            handleSubmit(formData, userId)
+          }}>
         <input placeholder={"Name"} name="name" onChange= {handleChange} value={name}/>
         <input placeholder={"Start Date"} name="startDate" onChange={handleChange}  value={startDate}/>
         <input placeholder={"End Date"} name="endDate" onChange={handleChange} value={endDate}/>
-        <input type="submit" value= "Create Trip" />
+        <input placeholder={"Length of Festival"} name="festivalLength" onChange={handleChange} value={festivalLength}/>
+        <input placeholder={"Ticket Cost"} name="ticketCost" onChange={handleChange} value={ticketCost}/>
+        <input placeholder={"Travel Group"} name="travelGroup" onChange={handleChange} value={travelGroup}/>
+        <input placeholder={"Name of Stay Location"} name="stayName" onChange={handleChange} value={stayName}/>
+        <input placeholder={"Stay Address"} name="stayAddress" onChange={handleChange} value={stayAddress}/>
+        <input placeholder={"Cost of Stay"} name="stayCost" onChange={handleChange} value={stayCost}/>
+
+        <input
+        type="submit"
+        value={editMode ? "Update Trip" : "Create Trip" }
+      />
     </form>
 )};
 
- const mapStateToProps = state => {
-    const userId =  state.currentUser ? state.currentUser.id : "" 
-     return {
-        formData: state.tripForm,
-        userId
+const mapStateToProps = state => {
+    const userId = state.currentUser ? state.currentUser.id : ""
+    return {
+      formData: state.tripForm,
+      userId
     }
-}
+  }
 
-export default connect(mapStateToProps, { updateTripForm, createTrip })(TripForm);
+export default connect(mapStateToProps, { updateTripForm })(TripForm);
 
 //start_date
 //end_date
